ec592fc10ce3f8cac9c7a8fdb438290fc9975875
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-02-10 21:53:48 +0000

    Refactor split pair calculation

--- algorithms/closest_pair.py
+++ algorithms/closest_pair.py
@@ -50,26 +50,33 @@ def _calculate_closest_split_pair(x_sorted, y_sorted, delta):
         if point[0] >= mid_x - delta
         and point[0] <= mid_x + delta
     ]
+
     smallest_distance = delta
     closest_pair = (None, None, float("inf"))
+
     for i in range(0, len(potential_closest_split_pairs)-1):
         for j in range(1, min(7, len(potential_closest_split_pairs)-i)):
-            point_1 = potential_closest_split_pairs[i]
-            point_2 = potential_closest_split_pairs[i + j]
-            distance = ((point_2[0] - point_1[0]) ** 2 + (point_2[1] - point_1[1]) ** 2) ** 0.5
+            distance = _distance_between(
+                potential_closest_split_pairs[i],
+                potential_closest_split_pairs[i + j]
+            )
             if distance < smallest_distance:
                 smallest_distance = distance
-                closest_pair = [potential_closest_split_pairs[i], potential_closest_split_pairs[i + j], distance]
+                closest_pair = (
+                    potential_closest_split_pairs[i],
+                    potential_closest_split_pairs[i + j],
+                    distance,
+                )
     return closest_pair
 
 def _brute_force_closest_pair(input_points):
     smallest_distance = float("inf")
-    closest_pair = None
     for i in range(0, len(input_points)-1):
         for j in range(i+1, len(input_points)):
-            if _distance_between(input_points[i], input_points[j]) < smallest_distance:
-                smallest_distance = _distance_between(input_points[i], input_points[j])
-                closest_pair = (input_points[i], input_points[j], smallest_distance)
+            distance = _distance_between(input_points[i], input_points[j])
+            if  distance < smallest_distance:
+                smallest_distance = distance
+                closest_pair = (input_points[i], input_points[j], distance)
     return closest_pair
 
 def _distance_between(point_1, point_2):
@@ -78,10 +85,10 @@ def _distance_between(point_1, point_2):
 if __name__ == '__main__':
     random.seed(100)
     input_points = [(x, y) for x, y in zip((randint(0, 50) for x in range(1000)), (randint(0, 50) for y in range(1000)))]
-    # print(input_points)
-    t_start = datetime.now()
-    print(closest_pair(list(set(input_points))))
-    t_finish = datetime.now()
 
-    taken = t_finish - t_start
+    # t_start = datetime.now()
+    print(closest_pair(list(set(input_points))))
+    # t_finish = datetime.now()
+
+    # taken = t_finish - t_start
     # print(taken)
