98f819c754d4e86b935fedc90fd7c5ee44c40bbb
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-02-10 16:21:03 +0000

    Create test directory

---
+++ tests/test_closest_pair.py
@@ -0,0 +1,8 @@
+from algorithms import closest_pair
+
+class TestClosestPair():
+    def test_the_test(self):
+        assert 1 + 1 == 2
+
+    def test_not_the_test(self):
+        assert 1 + 1 == 5
