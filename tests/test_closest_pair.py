109c93aa5d349716a92f32862e0521d15ed5c991
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-02-10 16:49:33 +0000

    Add tests for closest_pair

--- tests/test_closest_pair.py
+++ tests/test_closest_pair.py
@@ -1,8 +1,40 @@
-from algorithms import closest_pair
+from random import shuffle
+from algorithms.closest_pair import closest_pair
 
 class TestClosestPair():
-    def test_the_test(self):
-        assert 1 + 1 == 2
+    def test_closest_pair_and_distance_are_found_for_x_in_first_half(self):
+        input_points = [(2, 3), (3, 4), (5, 1), (12, 10), (12, 30), (40, 50)]
 
-    def test_not_the_test(self):
-        assert 1 + 1 == 5
+        closest_pair_result = closest_pair(input_points)
+
+        assert closest_pair_result[0] == (2, 3)
+        assert closest_pair_result[1] == (3, 4)
+        assert closest_pair_result[2] == 1.4142135623730951
+
+    def test_closest_pair_and_distance_are_found_for_x_in_second_half(self):
+        input_points = [(5, 1), (12, 10), (12, 30), (40, 50), (41, 3), (42, 4)]
+
+        closest_pair_result = closest_pair(input_points)
+
+        assert closest_pair_result[0] == (41, 3)
+        assert closest_pair_result[1] == (42, 4)
+        assert closest_pair_result[2] == 1.4142135623730951
+
+    def test_closest_pair_and_distance_are_found_for_split_pair(self):
+        input_points = [(19, 15), (33, 35), (36, 34), (36, 45), (39, 21), (40, 36)]
+
+        closest_pair_result = closest_pair(input_points)
+
+        assert closest_pair_result[0] == (33, 35)
+        assert closest_pair_result[1] == (36, 34)
+        assert closest_pair_result[2] == 3.1622776601683795
+
+    def test_input_order_does_not_matter(self):
+        input_points = [(2, 3), (3, 4), (5, 1), (12, 10), (12, 30), (40, 50)]
+        shuffle(input_points)
+        
+        closest_pair_result = closest_pair(input_points)
+
+        assert closest_pair_result[0] == (2, 3)
+        assert closest_pair_result[1] == (3, 4)
+        assert closest_pair_result[2] == 1.4142135623730951
